18b57909c5b4e97b8a31b33e1738df701fbb5f75
Olivia Ifebuche <oliviaifebuche002@gmail.com>
2026-05-08 21:38:47 -0700

    Add page components for appointments, departments, doctors, nurses, patients, settings, and staffs; create a custom 404 Not Found page

---
+++ app/not-found.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import "./globals.css";
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body>
+        <section className="flex items-center justify-center min-h-screen bg-blue-200/25 px-6">
+          <div className="text-center max-w-md">
+            <h1
+              className="text-9xl italic font-extrabold text-blue-700"
+            >
+              404
+            </h1>
+            <p className="mt-4 text-2xl font-semibold text-secondary">
+              Page Not Found
+            </p>
+            <p className= "mt-2 ">
+              Sorry, the page you’re looking for doesn’t exist or has been
+              moved.
+            </p>
+
+            <div className="mt-8 flex justify-center gap-4">
+              <Link
+                href="/"
+                className="px-6 py-3 rounded-lg bg-red-600 font-semibold text-amber-50 hover:bg-red-700 transition duration-400 ease-in-out"
+              >
+                Go back Home
+              </Link>
+              
+            </div>
+          </div>
+        </section>
+      </body>
+    </html>
+  );
+}
